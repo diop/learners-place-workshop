8b937465905708e98e0befeec3f9c5706c309048
diop <fodesdiop@gmail.com>
2018-11-10 07:04:55 -0800

    correct app typos

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
+require('dotenv').config()
 const Tx = require('ethereumjs-tx')
 const Web3 = require('web3')
 const web3 = new Web3('https://ropsten.infura.io/CTNrMRz6lyyxOxddWG7y')
 
-const acct1 = '0x50Ebe9ad50DCf1Be1A35570E29587fa9F6eCDB46'
-const acct2 = '0x4526762d9FD955b56A85480239e7102c1484C222'
+const acct1 = '0x4526762d9FD955b56A85480239e7102c1484C222'
+const acct2 = '0x9870d54f9606e9103F91fcE6241033923fb2fCEd'
 
 // To not reveal private keys export them to environment variables.
 // Example: $ export PRIVATE_KEY_1 ='Private Key'
@@ -16,7 +17,7 @@ web3.eth.getTransactionCount(acct1, (err, txCount) => {
     const txObject = {
         nonce: web3.utils.toHex(txCount),
         to: acct2,
-        value: web3.utis.toHex(web3.utils.toWei('0.1', 'ether')),
+        value: web3.utils.toHex(web3.utils.toWei('0.1', 'ether')),
         gasLimit: web3.utils.toHex(21000),
         gasPrice:  web3.utils.toHex(web3.utils.toWei('10', 'gwei'))
     }
@@ -24,13 +25,13 @@ web3.eth.getTransactionCount(acct1, (err, txCount) => {
 
     // Sign a transaction
     const tx = new Tx(txObject)
-    tx.sign(privageKey1).serialize()
+    tx.sign(privateKey1)
 
-    const serializedTransaction = tx.serialize
+    const serializedTransaction = tx.serialize()
     const raw = '0x' + serializedTransaction.toString('hex')
 
     // Broadcast a transaction
-    web3.eth.sendSignedtransaction(raw, (err, txHash) => {
+    web3.eth.sendSignedTransaction(raw, (err, txHash) => {
         console.log('Transaction Hash: ', txHash)
     }) 
 })
